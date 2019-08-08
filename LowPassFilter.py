874e5a0922e506dddc89183ccaa0b2d7957e8513
Amol Pant <amol.pant@keysight.com>
2019-08-07 17:28:32 -0700

    Cleaning up a working prototype.

--- LowPassFilter.py
+++ LowPassFilter.py
@@ -1,61 +1,85 @@
 import numpy as np
 from scipy.signal import butter, lfilter, freqz
 import matplotlib.pyplot as plt
+import math
+
+class DownConversion():
+
+    def __init__(self, signal_data, sampling_rate, cuttoff_freq, window_size, time):
+        self.data = signal_data
+        self.fs = sampling_rate
+        self.cutoff = cuttoff_freq
+        self.order = window_size
+        self.T = time
+
+    def decompose(self):
+
+        # Get the filter coefficients so we can check its frequency response.
+        def butter_lowpass(cutoff, fs, order=5):
+            nyq = 0.5 * fs
+            normal_cutoff = cutoff / nyq
+            b, a = butter(order, normal_cutoff, btype='low', analog=False)
+            return b, a
+
+        def butter_lowpass_filter(data, cutoff, fs, order=5):
+            b, a = butter_lowpass(cutoff, fs, order=order)
+            y = lfilter(b, a, data)
+            return y
+
+        b, a = butter_lowpass(self.cutoff, self.fs, self.order)
+
+        print(len(b))
+        print(len(a))
+
+        w, h = freqz(b, a, worN=8000)
+        # plt.subplot(2, 1, 1)
+        # plt.plot(0.5*self.fs*w/np.pi, np.abs(h), 'b')
+        # plt.plot(self.cutoff, 0.5*np.sqrt(2), 'ko')
+        # plt.axvline(self.cutoff, color='k')
+        # plt.xlim(0, 0.5*self.fs)
+        # plt.title("Lowpass Filter Frequency Response")
+        # plt.xlabel('Frequency [Hz]')
+        # plt.grid()
+        n = int(self.T * self.fs) # total number of samples
+        t = np.linspace(0, self.T, n, endpoint=False)
+
+        # self.data = np.sin(1000000000*t*np.pi) + np.cos(200000000*t*np.pi)
+
+        data_i = self.data * np.sin(30000000*t*2*np.pi)
+        data_q = self.data * np.cos(30000000*t*2*np.pi)
+
+        y = butter_lowpass_filter(data_i, self.cutoff, self.fs, self.order)
+        z = butter_lowpass_filter(data_q, self.cutoff, self.fs, self.order)
 
 
-def butter_lowpass(cutoff, fs, order=5):
-    nyq = 0.5 * fs
-    normal_cutoff = cutoff / nyq
-    b, a = butter(order, normal_cutoff, btype='low', analog=False)
-    return b, a
+        #Plotting Stuff
+        # plt.subplot(2, 1, 2)
+        # plt.plot(t, self.data, 'b-', label='data')
+        # plt.plot(t, data_i, 'r-', label='I')
+        # plt.plot(t, data_q, 'g-', label='Q')
+        # plt.xlabel('Time [sec]')
+        # plt.grid()
+        # plt.legend()
+        #
+        # plt.subplot(2,2,1)
+        # plt.plot(t, y, 'g-', linewidth=2, label='filtered data_I')
+        # plt.plot(t, z, 'y-', linewidth=2, label='filtered data_Q')
+        # plt.xlabel('Time [sec]')
+        # plt.grid()
+        # plt.legend()
+        # #
+        # plt.subplot(2,2,2)
+        # amp = np.zeros(len(self.data))
+        # phases = np.zeros(len(self.data))
+        # for i in range(len(data_i)-1):
+        #     amp[i] = math.sqrt(data_i[i]**2 + data_q[i]**2)
+        #     phases[i] = math.atan(data_q[i]/data_i[i])
+        # plt.plot(t, amp, 'b-', label='Amplitude')
+        # plt.plot(t, phases, 'r-', label='Phase')
+        # plt.xlabel('Time [sec]')
+        # plt.grid()
+        # plt.legend()
+        # plt.subplots_adjust(hspace=0.35)
+        # plt.show()
 
-def butter_lowpass_filter(data, cutoff, fs, order=5):
-    b, a = butter_lowpass(cutoff, fs, order=order)
-    y = lfilter(b, a, data)
-    return y
-
-
-# Filter requirements.
-order = 7
-fs = 200*1000000       # sample rate, Hz
-cutoff = 10*100000  # desired cutoff frequency of the filter, Hz
-
-# Get the filter coefficients so we can check its frequency response.
-b, a = butter_lowpass(cutoff, fs, order)
-
-# Plot the frequency response.
-w, h = freqz(b, a, worN=8000)
-plt.subplot(2, 1, 1)
-plt.plot(0.5*fs*w/np.pi, np.abs(h), 'b')
-plt.plot(cutoff, 0.5*np.sqrt(2), 'ko')
-plt.axvline(cutoff, color='k')
-plt.xlim(0, 0.5*fs)
-plt.title("Lowpass Filter Frequency Response")
-plt.xlabel('Frequency [Hz]')
-plt.grid()
-
-
-# # Demonstrate the use of the filter.
-# # First make some data to be filtered.
-T = 5.0/400000         # seconds
-n = int(T * fs) # total number of samples
-t = np.linspace(0, T, n, endpoint=False)
-# # "Noisy" data.  We want to recover the 1.2 Hz signal from this.
-# data = np.sin(1.2**10000002*np.pi*t) + 1.5*np.cos(9*2*1000000*np.pi*t) + 0.5*np.sin(12.0**10000002*np.pi*t)
-def func(x):
-    return np.sin(1000000*x*np.pi)
-    # return (np.sin(1000000 * np.pi * x/2000000)) * (np.cos(1000000 * np.pi * x/2000000))
-data = np.sin(10000000*t*np.pi) + np.cos(2000000*t*np.pi)
-#
-# # Filter the data, and plot both the original and filtered signals.
-y = butter_lowpass_filter(data, cutoff, fs, order)
-#
-plt.subplot(2, 1, 2)
-plt.plot(t, data, 'b-', label='data')
-plt.plot(t, y, 'g-', linewidth=2, label='filtered data')
-plt.xlabel('Time [sec]')
-plt.grid()
-plt.legend()
-#
-plt.subplots_adjust(hspace=0.35)
-plt.show()
+        return amp, phases
--- QubitStateSimulation.py
+++ QubitStateSimulation.py
@@ -9,12 +9,18 @@ It will read in the phase and amplitude of a microwave drive that has been downc
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from pylab import *
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 from qutip import *
 from qutip.ipynbtools import version_table
 from SimDataGenerator import *
 from bloch_sphering import Animate
+from LowPassFilter import DownConversion
+from mpl_toolkits.mplot3d import proj3d
+from matplotlib.patches import FancyArrowPatch
+
+
 
 
 q = Qobj([[1], [0]])     #initializes a qubit into the state 0
@@ -60,7 +66,34 @@ def rSimS(sObj,InitialState): #Here we define a recursive function to track the 
         state[i+1]=rz(-sObj.Phases[i])*state[i+1]
     return state
 
-a=rSim(simD1,q)
+def rSimCustom(Amps,Phases,InitialState): #Here we define a recursive function to track the evolution from a detuned signal
+    state = np.array([InitialState])
+    for i in range(len(Amps)):
+        state = np.append(state,[r(Amps[i],Phases[i])*state[-1]])
+    return state
+
+order = 8
+fs =  1000000000    # sample rate, Hz
+cutoff = 20*1000000   # desired cutoff frequency of the filter, Hz
+T = 5.0/400000         # seconds
+n = int(T * fs) # total number of samples
+t = np.linspace(0, T, n, endpoint=False)
+
+data = 0.05*np.sin(40000000*t*2*np.pi)
+
+decomp = DownConversion(data,fs,cutoff,order,T)
+amp, phases = decomp.decompose()
+
+print(amp[0])
+print(phases[0])
+print(simD1.Amps[1])
+print(simD1.Phases[1])
+
+draw = r(0.4,-1.2567)*q
+
+amp = amp[1:1001]
+phases = phases[1:1001]
+a=rSimCustom(amp,phases,q)
 
 front_end = Animate(a)
 front_end.animation()
